0008779c7512a7517fefd6d9efedd4ab594ea446
Douglas Morais <dughlaisdd@gmail.com>
2024-09-11 12:11:04 -0300

    Config(Front end api connection also minor and initial config changes)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route>
+
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
---
+++ frontend/src/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'http://localhost:3002',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+type Data = {
+    username: string
+    email: string
+    password: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const submitData = async (data: Data) => {
+    try {
+        const response = await axios.post('http://localhost:3002/endpoint', data);
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+})
+
+export default api;
