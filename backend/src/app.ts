e4e5f8c2e95025a4ed3fbcd502653feebff42044
Douglas Morais <dughlaisdd@gmail.com>
2024-09-12 14:48:13 -0300

    Config/Feature(Frontend backend connection minor fixes and improviments | working well res and req)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,15 @@ import express, { Application } from "express";
 import routes from "../src/router";
 import cors from 'cors';
 
+const PORT = process.env.PORT
 const app: Application = express();
 
 app.set('trust proxy', true);
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: `http://localhost:3000`,
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
+    allowedHeaders: 'Content-Type,Authorization',
 }));
 
 app.use(express.json());
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -16,20 +16,19 @@ type Data = {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const submitData = async (data: Data) => {
     try {
-        const response = await axios.post('http://localhost:3002/endpoint', data);
+        const response = await axios.post('http://localhost:3002/register', data);
         console.log('Response:', response.data);
     } catch (error) {
         console.error('Error:', error);
     }
-
 }
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-})
+});
 
 export default api;
