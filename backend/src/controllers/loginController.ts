72af03372203015ab145acfc6c84123efdb62e7d
Douglas Morais <dughlaisdd@gmail.com>
2024-09-14 16:11:59 -0300

    Config/Change(Login controller jwt paylod change)

--- backend/src/controllers/loginController.ts
+++ backend/src/controllers/loginController.ts
@@ -6,10 +6,31 @@ const secretKey = process.env.JWT_SECRET || "";
 
 export default class loginController {
     async login (req: Request, res: Response) {
-        const { email, password } = req.body
+        const { email, username, password } = req.body
 
         try {
-            const token = jwt.sign( { email: email}, secretKey, { expiresIn: '1h'});
+
+            const userId = await prisma.user.findFirst({
+                where: {
+                    OR: [
+                        { email },
+                        { username }
+                    ]
+                },
+                select: { userId: true }
+            });
+
+            if ( userId === null ) {
+                return res.status(401).json({
+                    message: "Authentication failed"
+            })
+        }
+            const payLoad = {
+                sub: userId,
+                iat: Math.floor(Date.now() / 1000)
+            };
+
+            const token = jwt.sign(payLoad, secretKey, { expiresIn: '1h'});
 
             return res.status(200).json({
                 message: "Authentication sucessful",
